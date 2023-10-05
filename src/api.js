0477876fa108de774949a922f06ce7544c3ea485
Prantik45 <86847905+Prantik45@users.noreply.github.com>
2023-10-06 00:42:05 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'a2d6b7b382mshfc7f7a80bbdd17bp1d7b6cjsn41fd1225bddc',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+export const WEATHER_API_KEY = "878a4f4148a75da1bc8f48671ae42bd4";
